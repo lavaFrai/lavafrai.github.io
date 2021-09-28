c1e677eaa15f789ddd954696913ea4e0a598b42c
lavaFrai <vladimirkurdiukovv@yandex.ru>
2021-09-28 22:55:23 +0300

    0.0.2

--- codec/cp1251.js
+++ codec/cp1251.js
@@ -1,4 +1,7 @@
-function cp1251_encode()
+cp1251 = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ЂЃ‚ѓ„…†‡€‰Љ‹ЊЌЋЏђ‘’“”•–— ™љ›њќћџ ЎўЈ¤Ґ¦§Ё©Є«¬ ®Ї°±Ііґµ¶·ё№є»јЅѕ"
+
+
+function cp1251_encode(data)
 {
-    
+    return data + "!?";
 }
