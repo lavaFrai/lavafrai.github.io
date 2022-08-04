46e23d09177f7d14e7fd982d898c57f7d24c1873
lavaFrai <vladimirkurdiukovv@yandex.ru>
2022-08-04 21:59:27 +0300

    Initial commit

--- codec/cp1251.js
+++
@@ -1,7 +0,0 @@
-cp1251 = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ЂЃ‚ѓ„…†‡€‰Љ‹ЊЌЋЏђ‘’“”•–— ™љ›њќћџ ЎўЈ¤Ґ¦§Ё©Є«¬ ®Ї°±Ііґµ¶·ё№є»јЅѕ"
-
-
-function cp1251_encode(data)
-{
-    return data + "!?";
-}
