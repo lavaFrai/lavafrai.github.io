32fdb2cc11284ff501e35abd058d45a00d245b63
lavaFrai <vladimirkurdiukovv@yandex.ru>
2022-08-05 21:07:59 +0300

    Initial commit

---
+++ script/stars-rating.js
@@ -0,0 +1,20 @@
+function isStringDigit(str) {
+    return /^\d+$/.test(str);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var stars = document.querySelectorAll('.stars-rating');
+    for (var i = 0; i < stars.length; i++) {
+        let value = stars[i].getAttribute('value');
+        if (value == null || !isStringDigit(value)) {
+            value = '0';
+        }
+
+        value = parseInt(value);
+
+        console.log(value);
+
+        stars[i].innerText = '★'.repeat(value) + '☆'.repeat(5 - value);
+    }
+});
